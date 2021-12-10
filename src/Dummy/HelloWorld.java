11ea1ab35da594f848def2bbfa428a71a52d7e30
ssaini07 <78027352+ssaini07@users.noreply.github.com>
2021-12-10 19:20:11 +0530

    Refractor1 added successfully

--- src/Dummy/HelloWorld.java
+++ src/Dummy/HelloWorld.java
@@ -3,7 +3,10 @@ package Dummy;
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		System.out.println("Hello world!!!");
+		System.out.println("Hello world");
+		System.out.println("Hello how are you there!!");
+		System.out.println("My name is shubham saini");
+		System.out.println("Proud to be an indian!!");
 
 	}
 
